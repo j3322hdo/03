d3c62b89e6808f264e300beebc67a1c77f931afe
Genki Kai <j3322hdo@sit.ac.jp>
2024-12-10 15:22:14 +0900

    initial import

---
+++ load_dataset.py
@@ -0,0 +1,28 @@
+import torch
+import matplotlib.pyplot as plt
+from torchvision import datasets
+import torchvision.transforms.v2 as transforms
+
+ds_train = datasets.FashionMNIST(
+    root = 'data',
+    train = True,
+    download = True,
+)
+
+print(f"numbers of datesets:{len(ds_train)}")
+image, target = ds_train[0]
+
+print(type(image),target)
+
+#plt.imshow(image)
+#plt.title(target)
+#plt.show()
+
+image_tansor = transforms.functional.to_image(image)
+print(image_tansor.shape, image_tansor.dtype)
+
+image = transforms.functional.to_image(image)
+image = transforms.functional.to_dtype(image, dtype=torch.float32, scale=True)
+print(image.shape, image.dtype)
+print(image.min(), image.max())
+print()
